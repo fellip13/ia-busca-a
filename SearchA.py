26d9331cf79cee118d4b75c1d97bcd2f5e83bef3
Fellip da Silva Ribeiro <51681792+fellip13@users.noreply.github.com>
2022-10-24 03:06:46 -0300

    SearchA corrigido
    Falta apenas decidir como será a entrada do usuário e como será impressa a saída.

--- SearchA.py
+++ SearchA.py
@@ -1,142 +1,141 @@
-#182043765
+class TreeNode:
+    def __init__(self, puzzle, cost, order="N/A"):
+        self.puzzle = puzzle
+        self.cost = cost
+        self.children = []
+        self.order = order
+        self. parent = None
 
-def moves(position):
-    moves = []
-    for x in [1, -1]:
-        if position[0] + x >= 0 and position[0] + x <= 2:
-            moves.append([position[0] + x, position[1]])
-        if position[1] + x >= 0 and position[1] + x <= 2:
-            moves.append([position[0], position[1] + x])
-    return moves
+    def add_child(self, child):
+        self.child = child
+        child.parent = self
+        self.children.append(child)
 
-def positionOfZero(matriz):
+    def get_level(self):
+        level = 0
+        p = self.parent
+        while p:
+            p = p.parent
+            level += 1
+        return level
+
+    def print_tree(self):
+        print('  ' * self.get_level() + '|-- ', end='')
+        print(f"{self.puzzle} --- {self.cost} --- {self.get_level()} --- {self.order}"
+              f"{'°' * int(self.order != 'N/A')}{' --- Resolvido!' * int(self.cost == 0)}")
+        if self.children:
+            for each in self.children:
+                each.print_tree()
+
+    def next_search_node(self, best_node, limit_cost):
+        if len(self.children) == 0:
+            if self.cost + self.get_level() < best_node.cost + best_node.get_level() and \
+                    self.cost + self.get_level() <= limit_cost:
+                return self
+            return best_node
+        else:
+            for each in self.children:
+                result = each.next_search_node(best_node, limit_cost)
+                if result.cost + result.get_level() < best_node.cost + best_node.get_level() and \
+                        result.cost + result.get_level() <= limit_cost:
+                    best_node = result
+            return best_node
+
+    def search_node(self, puzzle):
+        if self.puzzle == puzzle:
+            return True
+        for each in self.children:
+            result = False or each.search_node(puzzle)
+            if result:
+                return result
+        return False
+
+    def count_nodes(self, nodes):
+        nodes += 1
+        if self.children:
+            for each in self.children:
+                nodes = each.count_nodes(nodes)
+        return nodes
+
+    def expanded_nodes(self, nodes_expanded):
+        if len(self.children) != 0:
+            nodes_expanded += 1
+            for each in self.children:
+                nodes_expanded = each.expanded_nodes(nodes_expanded)
+        return nodes_expanded
+
+
+def possible_moves_zero(position_zero):
+    moves_zero = [[-1, 0], [1, 0], [0, 1], [0, -1]]  # [cima, baixo, direita, esquerda]
+    if (position_zero[1] == 0):  # borda esquerda
+        moves_zero.pop(3)
+    if (position_zero[1] == 2):  # borda direita
+        moves_zero.pop(2)
+    if (position_zero[0] == 2):  # borda inferior
+        moves_zero.pop(1)
+    if (position_zero[0] == 0):  # borda superior
+        moves_zero.pop(0)
+    return moves_zero
+
+
+def position_zero(matriz):
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
             if matriz[i][j] == 0:
-                return [i, j] 
+                return [i, j]
 
-def dist(initial: list, final: list):
-    return abs(initial[0] - final[0]) + abs(initial[1] - final[1])
 
-def originalPosition(value):
-    positions = [[2,2], [0,0], [0,1], [0,2], [1,0], [1,1], [1,2], [2,0], [2,1]]
-    return positions[value]
-
-def state(matriz):
-    state = 0
+def cost(matriz):
+    cost = 0
+    positions = [[2, 2], [0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1]]
     for i in range(len(matriz)):
         for j in range(len(matriz[i])):
             value = matriz[i][j]
             if (value == 0):
                 continue
-            position = originalPosition(value)
-            d = dist([i, j], position)
-            state += d
-            #print(f'Value {value} [{i},{j}] original position {position} dist {d}')
-    return state
+            position = positions[value]
+            cost += abs(i - position[0]) + abs(j - position[1])
+    return cost
 
-def printMatriz(matriz):
-    for i in range(len(matriz)):
-        for j in range(len(matriz[i])):
-            value = matriz[i][j]
-            print(f'{value}', end = ' ')
-        print()
-    print()
 
-def readMatriz(file: str):
-   with open(file, 'r') as file:
-    matriz = [[int(number) for number in line.split(',')] for line in file]
+def apply_move(matriz, move, position_zero):
+    i_new_zero = position_zero[0] + move[0]
+    j_new_zero = position_zero[1] + move[1]
+    number_to_swap = matriz[i_new_zero][j_new_zero]
+    matriz[position_zero[0]][position_zero[1]] = number_to_swap
+    matriz[i_new_zero][j_new_zero] = 0
     return matriz
 
-def swap(matriz, first, second):
-    aux = matriz[first[0]][first[1]]
-    matriz[first[0]][first[1]] = matriz[second[0]][second[1]]
-    matriz[second[0]][second[1]] = aux
+
+def solve_puzzle(node, root, previous):
+    previous += 1
+    node.order = previous
+    zero_position = position_zero(node.puzzle)
+    moves_zeros = possible_moves_zero(zero_position)
+    for move in moves_zeros:
+        new_puzzle = [node.puzzle[0].copy(), node.puzzle[1].copy(), node.puzzle[2].copy()]
+        new_puzzle = apply_move(new_puzzle, move, zero_position)
+        cost_new_puzzle = cost(new_puzzle)
+        if root.search_node(new_puzzle) == False:
+            newNode = TreeNode(new_puzzle, cost_new_puzzle)
+            node.add_child(newNode)
+    next_node = root.next_search_node(newNode, node.get_level() + node.cost)
+    if next_node.cost != 0 and root.count_nodes(0) <= 10000:
+        solve_puzzle(next_node, root, previous)
+
 
 def main():
-    file = 'puzzle.txt'
-    puzzle = readMatriz(file)
-    
-    #printMatriz(puzzle)
-    #print(f'Initial state {state(puzzle)}')
-
-    print(state([[1,2,0], [4,8,3], [7,6,5]]))
-    print(state([[0,1,2], [4,8,3], [7,6,5]]))
-    print(state([[1,8,2], [4,6,3], [7,5,0]]))
-    print(state([[1,8,2], [4,6,3], [0,7,5]]))
-
-
-    maxInterantion = 0
-    while(True):
-        if (state(puzzle) == 0):
-            print("solved")
-            break
-
-        if (maxInterantion == 10000):
-            print("Unresolvable")
-            break
-        
-        zeroPosition = positionOfZero(puzzle)
-        movesOfZero = moves(zeroPosition)
-
-        bestPuzzle = None
-        for valuePosition in movesOfZero:
-            #print('puzzle')
-            #printMatriz(puzzle)
-            newPuzzle = [puzzle[0].copy(), puzzle[1].copy(), puzzle[2].copy()]
-            swap(newPuzzle, zeroPosition, valuePosition)
-
-            print(state(newPuzzle))
-            printMatriz(newPuzzle)
-            if bestPuzzle is None or state(newPuzzle) < state(puzzle):
-                bestPuzzle = [newPuzzle[0].copy(), newPuzzle[1].copy(), newPuzzle[2].copy()]
-            #print(f'Future state {state(newPuzzle)}')
-            #printMatriz(newPuzzle)
-
-        puzzle = [bestPuzzle[0].copy(), bestPuzzle[1].copy(), bestPuzzle[2].copy()]
-        #print('final')
-        #printMatriz(puzzle)
-        #print(f'Interaction {maxInterantion}')
-        maxInterantion += 1
-
-    '''count = 0
-    while(True):
-        if (state(puzzle) == 0 or count == 3):
-            break
-        zeroPosition = positionOfZero(puzzle)
-        movesOfZero = moves(zeroPosition)
-
-        bests = dict()
-        for valuePosition in movesOfZero:
-            if (valuePosition[0] == 0 and valuePosition[1] == 0):
-                continue
-
-            d = dist(valuePosition, originalPosition(puzzle[valuePosition[0]][valuePosition[1]]))
-            print(f'position: {valuePosition} value: {puzzle[valuePosition[0]][valuePosition[1]]} dist: {d}')
-            if d == 0:
-                continue
-            bests[d] = valuePosition
-        
-        countX = 0
-        best = None
-        for k, v in bests.items():
-            if countX == 0:
-                best = [k, v]
-                countX = 1
-            else:
-                if k < best[0]:
-                    best = [k, v]
-            
-        
-        print(f'zero position: {zeroPosition} moves: {movesOfZero} best: {best}')
-
-        aux = puzzle[zeroPosition[0]][zeroPosition[1]]
-        puzzle[zeroPosition[0]][zeroPosition[1]] = puzzle[best[1][0]][best[1][1]]
-        puzzle[best[1][0]][best[1][1]] = aux
-
-        printMatriz(puzzle)
-        count += 1
-    print(f'{count}')'''
+    puzzle = [[3,2,4], [1,0,6], [7,5,8]]
+    cost_puzzle = cost(puzzle)
+    root = TreeNode(puzzle=puzzle, cost=cost_puzzle)
+    solve_puzzle(node=root, root=root, previous=0)
+    print("\nÁrvore expandida: ")
+    print("|-- Puzzle configuration --- Cost --- Depth --- Expansion Order\n")
+    root.print_tree()
+    print("\nNós criados (incluindo o nó raiz): ", root.count_nodes(0))
+    print("Nós expandidos (incluindo o nó raiz): ", root.expanded_nodes(0))
 
 
-main()
+if __name__ == '__main__':
+    main()
+    pass
